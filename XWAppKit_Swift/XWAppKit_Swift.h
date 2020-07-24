b0bb884bcfeecc9f33beed7b0820a797e79c98db
zhuangxiaowei <zhuangxiaowei_dev@163.com>
2020-07-24 09:54:07 +0800

    init

---
+++ XWAppKit_Swift/XWAppKit_Swift.h
@@ -0,0 +1,19 @@
+//
+//  XWAppKit_Swift.h
+//  XWAppKit_Swift
+//
+//  Created by ZHXW on 2020/7/24.
+//  Copyright © 2020 ZhuanagXiaowei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XWAppKit_Swift.
+FOUNDATION_EXPORT double XWAppKit_SwiftVersionNumber;
+
+//! Project version string for XWAppKit_Swift.
+FOUNDATION_EXPORT const unsigned char XWAppKit_SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XWAppKit_Swift/PublicHeader.h>
+
+
