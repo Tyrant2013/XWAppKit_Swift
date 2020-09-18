6e9404176729754c3854a65c0646ee2df8bbd358
zhuangxiaowei <zhuangxiaowei_dev@163.com>
2020-09-18 16:06:30 +0800

    添加文件

---
+++ Sources/XWAppKit_Swift/UI/View/XWAKAnimatedImageView.swift
@@ -0,0 +1,13 @@
+//
+//  XWAKAnimatedImageView.swift
+//  XWAppKit_Swift
+//
+//  Created by ZHXW on 2020/9/18.
+//  Copyright © 2020 ZhuanagXiaowei. All rights reserved.
+//
+
+import UIKit
+
+public class XWAKAnimatedImageView: UIImageView {
+    
+}
---
+++ Sources/XWAppKit_Swift/UI/View/XWAKLabel.swift
@@ -0,0 +1,21 @@
+//
+//  XWAKLabel.swift
+//  XWAppKit_Swift
+//
+//  Created by ZHXW on 2020/9/18.
+//  Copyright © 2020 ZhuanagXiaowei. All rights reserved.
+//
+
+import UIKit
+
+class XWAKLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
