e30656d2ad5699e95d6fc0e057b04fe696bbd143
zhuangxiaowei <zhuangxiaowei_dev@163.com>
2020-10-20 15:56:59 +0800

    修改UIColor Extension的可见性

--- Sources/XWAppKit_Swift/Extension/UIColorExtension.swift
+++ Sources/XWAppKit_Swift/Extension/UIColorExtension.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-extension UIColor {
+public extension UIColor {
     class func xwak_color(with hex: Int, alpha: CGFloat) -> UIColor {
         if hex > 0xffffff || hex < 0 || alpha < 0 || alpha > 1 {
             return .clear
@@ -18,5 +18,26 @@ extension UIColor {
         let g = CGFloat(hex & 0x00ff00 >> 8) / 255
         let b = CGFloat(hex & 0x0000ff) / 255
         return UIColor(red: r, green: g, blue: b, alpha: alpha)
+    }
+    class func xwak_color(wiht hex: String) -> UIColor {
+        var hexValue = ""
+        if hex.starts(with: "0x") || hex.starts(with: "0X") {
+            hexValue = String(hex.dropFirst(2))
+        }
+        if !(hex.count == 8 || hex.count == 6) {
+            return .clear
+        }
+        let array = Array(hexValue)
+        let numbers = stride(from: 0, to: array.count, by: 2).map {
+            strtoul(String(array[$0..<min($0 + 2, array.count)]), nil, 16)
+        }
+        if numbers.count == 4 {
+            let red = CGFloat(numbers[0]) / 255.0
+            let green = CGFloat(numbers[1]) / 255.0
+            let blue = CGFloat(numbers[2]) / 255.0
+            let alpha = CGFloat(numbers[3]) / 255.0
+            return .init(red: red, green: green, blue: blue, alpha: alpha)
+        }
+        return .clear
     }
 }
