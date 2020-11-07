4f6d6b43ec0b5ff67eea9c360192db293aee95aa
Zhuang Xiaowei <zhuangxiaowei_dev@163.com>
2020-11-07 20:04:11 +0800

    添加颜色转化为16进制字符串

--- Sources/XWAppKit_Swift/Extension/UIColorExtension.swift
+++ Sources/XWAppKit_Swift/Extension/UIColorExtension.swift
@@ -41,3 +41,14 @@ public extension UIColor {
         return .clear
     }
 }
+
+public extension UIColor {
+    func toHex() -> String {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+        getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        return String(format: "0x%02X%02X%02X%02X", red, green, blue, alpha)
+    }
+}
