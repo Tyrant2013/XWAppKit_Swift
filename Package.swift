ee587c49fb75a149bba1e4be652666ae2a7d1f48
zhuangxiaowei <zhuangxiaowei_dev@163.com>
2020-11-17 18:14:33 +0800

    改成static

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "XWAppKit_Swift",
-            type: .dynamic,
+            type: .static,
             targets: ["XWAppKit_Swift"]),
     ],
     dependencies: [
