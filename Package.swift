906353798205f15396199e80f75ae2a71455c7a5
zhuangxiaowei <zhuangxiaowei_dev@163.com>
2020-11-23 11:03:37 +0800

    fix warning

--- Package.swift
+++ Package.swift
@@ -21,7 +21,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "XWAppKit_Swift",
-            dependencies: []),
+            dependencies: [],
+            resources: [.copy("Info.plist")]),
         .testTarget(
             name: "XWAppKit_SwiftTests",
             dependencies: ["XWAppKit_Swift"]),
