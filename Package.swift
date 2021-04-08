d24ef45394b62a9944ab0ca2ad8fb97da9748d3c
zhuangxiaowei <zhuangxiaowei_dev@163.com>
2021-04-08 09:58:57 +0800

    试试

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "XWAppKit-Swift",
+            name: "XWAppKit-Swift.framework",
             type: .dynamic,
             targets: ["XWAppKit-Swift"]),
     ],
