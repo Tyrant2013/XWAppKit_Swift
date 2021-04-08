3ba8b5c92b9bdf47d48ad7855cc5740042ce3ab2
zhuangxiaowei <zhuangxiaowei_dev@163.com>
2021-04-08 10:18:23 +0800

    再改

--- Package.swift
+++ Package.swift
@@ -10,7 +10,6 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "XWAppKit-Swift.framework",
-            type: .dynamic,
             targets: ["XWAppKit-Swift"]),
     ],
     dependencies: [
@@ -23,7 +22,8 @@ let package = Package(
         .target(
             name: "XWAppKit-Swift",
             dependencies: [],
-            exclude: ["Info.plist"]),
+            exclude: ["Info.plist"],
+            resources: [.copy("Resources.xcassets")]),
         .testTarget(
             name: "XWAppKit-SwiftTests",
             dependencies: ["XWAppKit-Swift"]),
